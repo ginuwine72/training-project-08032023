b4db7447c172ddff9ea88afb50c5bdfdb54c1f24
ardee <ardeeintern@gmail.com>
2023-04-20 10:47:22 +0700

    update

--- resources/views/showproduct.blade.php
+++ resources/views/showproduct.blade.php
@@ -144,25 +144,17 @@
       <nav aria-label="Breadcrumb">
         <ol role="list" class="mx-auto flex max-w-2xl items-c enter space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         @foreach ($product as $pd)
-      <td>
-        <div class="frame" onmouseover="document.getElementById('greeting').style.display = 'block';" onmouseout="document.getElementById('greeting').style.display = 'none';">
-  <div style="text-align: left;">
-  <img src="{{ asset('product/'.$pd['image']) }}" style="width: 188px; height: 188px;" alt="">
-
-    <h1 style="font-weight: bold; font-size: 15px;">{{$pd['name']}}</h1> 
-    {{$pd['description']}}<br>
-  
-    
-    <h1 style="color: grey; font-weight: bold; font-size: 10px; text-decoration: line-through; margin-right: 10px;">
-        ฿{{ number_format($pd['price'] + ($pd['price'] * 0.25), 2) }}
-    </h1>
-    <h1 style="color: red; font-weight: bold; font-size: 18px; display: flex; align-items: center;">
-        ฿{{$pd['price']}}
-        <span style="font-size: 12px; margin-left: 5px; color: green;">ส่วนลด 25%</span>
-    </h1>
-  
-  </div>
-  </div>
-  </td>
-
+<div class="frame" onmouseover="document.getElementById('greeting').style.display = 'block';" onmouseout="document.getElementById('greeting').style.display = 'none';">
+<div style="text-align: left;">
+<img src="{{ asset('product/'.$pd['image']) }}" style="width: 188px; height: 188px;" alt="">
+<h1 style="font-weight: bold; font-size: 15px;">{{$pd['name']}}</h1>
+{{$pd['description']}}<br>
+<h1 style="color: grey; font-weight: bold; font-size: 10px; text-decoration: line-through; margin-right: 10px;">
+฿{{ number_format($pd['price'] + ($pd['price'] * 0.25), 2) }}
+</h1>
+<h1 style="color: red; font-weight: bold; font-size: 18px; display: flex; align-items: center;">
+฿{{$pd['price']}}<span style="font-size: 12px; margin-left: 5px; color: green;">ส่วนลด 25%</span>
+</h1>
+</div>
+</div>
 @endforeach
