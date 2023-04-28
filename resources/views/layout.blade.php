28fd9ca5405c6169e4c9a88abb9dec0a7013cc7b
ardee <ardeeintern@gmail.com>
2023-04-28 13:23:21 +0700

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -42,7 +42,7 @@
                     @endif
                     <div class="row">
                         <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
-                        <a href="{{ route('cart') }}" class="btn btn-primary btn-block">View all</a>
+                        <a href="{{ route('cart') }}" class="btn btn-primary btn-block">View  all</a>
                         </div>
                     </div>
                 </div>
